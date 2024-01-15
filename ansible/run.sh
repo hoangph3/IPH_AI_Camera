2ef84b14c1a0e69f31a98b9b2a5ed47a05bf3fe3
duyktr <duy@kotora.vn>
2024-01-15 15:42:20 +0700

    add api and ipcam ansible

--- ansible/run.sh
+++
@@ -1,3 +0,0 @@
-docker run --rm --net=host \
--v $(pwd)/ansible:/data \
-hoangph3/ansible:$(uname -m)-2.10 ansible-playbook $1
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker run --rm --net=host \
--v $(pwd)/ansible:/data
-willhallonline/ansible:latest ansible-playbook $1
+-v $(pwd)/ansible:/data \
+hoangph3/ansible:$(uname -m)-2.10 ansible-playbook $1
