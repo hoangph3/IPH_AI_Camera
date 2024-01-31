238f19d95c7c451f3f72b5900f70b7be281aca49
DuyNHKTR <138202266+DuyNHKTR@users.noreply.github.com>
2024-01-31 23:42:49 +0700

    Create auto_prune_tracking.py

---
+++ reid/auto_prune_tracking.py
@@ -0,0 +1,9 @@
+from core.prune_log_tracking import PruneLogger
+from utility.hparams import get_hparams_from_file
+
+
+if __name__ == "__main__":
+    prune_config = get_hparams_from_file("./env/prod.json")
+    pruner = PruneLogger(config=prune_config)
+
+    pruner.run()
